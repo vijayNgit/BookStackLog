813dcdb9859ee70fe88a3060b09e58b42c4f8798
Vijay Singh Negi <hi2vsnegi@gmail.com>
2025-02-14 17:07:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ function increment() {
     count += 1
     countEl.textContent = count
 }
+function decrement() {
+    count -= 1
+    if (count < 0) {
+        count = 0
+    }
+    countEl.textContent = count
+}
 
 function save() {
     let countStr = count + " - "
